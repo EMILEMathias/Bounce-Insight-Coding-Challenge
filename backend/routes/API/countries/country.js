bf7ec8a8487be981c35fcd1265dba6771048cfb3
git config --global user.password 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 <mathias.emile@gmail.com>
2023-04-23 18:01:26 +0200

    feat:node handle errors

--- backend/routes/API/countries/country.js
+++ backend/routes/API/countries/country.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const axios = require('axios')
 
 router.get('/:countryName', async (req, res) => {
-    console.log(req.params)
-    const countryData = await axios.get(`https://restcountries.com/v3.1/name/${req.params.countryName}`)
-    console.log(countryData.data)
-    res.send(countryData.data)
+    try {
+        const countryData = await axios.get(`https://restcountries.com/v3.1/name/${req.params.countryName}`)
+        res.status(200).send(countryData.data)
+    } catch (error) {
+        res.status(400).send('Invalid country name')
+    }
 })
 
 module.exports = router
