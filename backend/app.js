e34e81feea0731f0f8f02bf39007901a6e43906e
EMILEMathias <mathias.emile@gmail.com>
2023-04-26 20:24:13 +0200

    feat: autocomplete input and api request

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,71 @@
-import { Box, Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Input } from "@chakra-ui/react";
 import "./styles/globals.css";
+import { CUIAutoComplete } from "chakra-ui-autocomplete";
+import React from "react";
+import axios from "axios";
+import countries from "./countries.json";
 
 function App() {
+  const [pickerItems, setPickerItems] = React.useState(countries);
+  const [selectedItems, setSelectedItems] = React.useState([]);
+  axios.get("http://localhost:3001/country/france").then((response) => {
+    console.log(response.data);
+  });
+
+  const handleCreateItem = (item) => {
+    setPickerItems((curr) => [...curr, item]);
+    setSelectedItems((curr) => [...curr, item]);
+  };
+
+  const handleSelectedItemsChange = (selectedItems) => {
+    if (selectedItems) {
+      setSelectedItems(selectedItems);
+    }
+  };
   return (
-    <Container bg={"rgb(30, 30, 34)"} color={"white"} m={0}>
-      <Flex alignItems={"center"} alignContent={"center"}>
-        <h1>Play Like Pro</h1>
+    <Container
+      bg={"rgb(30, 30, 34)"}
+      color={"white"}
+      maxW={"none"}
+      height={"100vh"}
+    >
+      <Flex
+        h={"full"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        flexDir={"column"}
+      >
+        <Heading as={"h1"} mb={"100px"}>
+          Country data
+        </Heading>
+        <Box maxW={"600px"} w={"600px"}>
+          <CUIAutoComplete
+            inputStyleProps={{
+              bg: "rgb(30, 30, 34)",
+            }}
+            listItemStyleProps={{ bg: "rgb(30, 30, 34)" }}
+            listStyleProps={{
+              bg: "rgb(30, 30, 34)",
+              maxH: "500px",
+            }}
+            toggleButtonStyleProps={{ bg: "rgb(30, 30, 34)", border: "1px" }}
+            tagStyleProps={{
+              bg: "rgb(30, 30, 34)",
+              color: "white",
+              border: "1px",
+              h: "30px",
+            }}
+            highlightItemBg={"rgb(40, 40, 44)"}
+            label="Looking for a country ? Search here !"
+            placeholder="Type a Country"
+            onCreateItem={handleCreateItem}
+            items={pickerItems}
+            selectedItems={selectedItems}
+            onSelectedItemsChange={(changes) =>
+              handleSelectedItemsChange(changes.selectedItems)
+            }
+          />
+        </Box>
       </Flex>
     </Container>
   );
